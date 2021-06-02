725b6d69c886c782cf57d5d5070925796dce3930
Ben Hayes <b.j.hayes@qmul.ac.uk>
2021-06-02 16:34:17 +0100

    Fix .flac file bg

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 // Serves static directory when 'static' is included in pathname
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
+// Serve .flac files when requested
+app.get("/stimuli_HugginsPitch/*.flac", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "static/" + req.originalUrl));
+});
+
 // This ensures that any path is routed to index.html
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));

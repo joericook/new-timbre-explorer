ec34a0c7d414077538647245d4ef1683a54e916e
joericook <joe.ri.cook@outlook.com>
2021-06-22 17:55:47 +0100

    Added Feedback 3

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -16,6 +16,7 @@ import Training2 from "./views/Training2.js";
 import Testing2 from "./views/Testing2.js";
 import Feedback2 from "./views/Feedback2.js";
 import Testing3 from "./views/Testing3.js";
+import Feedback3 from "./views/Feedback3.js";
 
 // Enables forward and back navigation with historyAPI
 const navigateTo = url => {
@@ -41,6 +42,7 @@ const router = async () => {
         { path: "/testing2", view: Testing2 },
         { path: "/feedback2", view: Feedback2 },
         { path: "/testing3", view: Testing3 },
+        { path: "/feedback3", view: Feedback3 },
     ];
     
     // Test each route for potential match
@@ -69,7 +71,7 @@ const router = async () => {
     // Some paths send html to overlay
     // others send html to taskContainer
     if (['/', '/info', '/consent', '/consentdl', '/hearing', '/headphones', '/background', '/testing1', '/feedback1',
-         '/testing2', '/feedback2', '/testing3'].includes(match.route.path)) {
+         '/testing2', '/feedback2', '/testing3', '/feedback3'].includes(match.route.path)) {
         console.log("overlayContent");
         document.querySelector("#overlayContent").innerHTML = await view.getHtml();
         document.getElementById("overlay").style.display = "block";
---
+++ frontend/static/js/views/Feedback3.js
@@ -0,0 +1,202 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("Feedback3");
+    }
+
+    async getHtml() {
+        return `
+            <div class="infoContainer">
+                <h1>Task 3 Feedback</h1>
+                <br>
+                <p>
+                    Before continuing, please answer the following questions about the task you just completed:
+                </p>
+                <br>
+                <form id="feedback3Form">
+                    <div class="roundedBox">
+                        <label>How challenging was it for you to do this task?:</label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="task2Difficulity" value="1" required>
+                                <label>Very Easy</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Difficulity" value="2" required>
+                                <label>Easy</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Difficulity" value="3" required>
+                                <label>Neutral</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Difficulity" value="4" required>
+                                <label>Challenging</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Difficulity" value="5" required>
+                                <label>Very Challenging</label>
+                            </li>
+                        </ul>
+
+                        <label>How much did you enjoy the task you just completed?:</label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="task2Enjoyment" value="1" required>
+                                <label>Not at all</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Enjoyment" value="2" required>
+                                <label>Slightly</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Enjoyment" value="3" required>
+                                <label>Somewhat</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Enjoyment" value="4" required>
+                                <label>Moderately</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="task2Enjoyment" value="5" required>
+                                <label>Very Much</label>
+                            </li>
+                        </ul>
+                    </div>
+                    <br>
+                    <p>
+                        The following questions concern the Timbre Explorer Interface as a whole:
+                    </p>
+                    <br>
+                    <div class="roundedBox">
+                        <p>
+                            For each of the following, read the description and rate how well the description is demonstrated by the Timbre Explorer. 
+                            How much did using this tool increase your understanding of each concept and how?:
+                        </p>
+                        <br>
+                        <label>
+                            <strong>Timbre (sound aspect)</strong> - Timbre is a multidimensional aspect of sound that accounts for non-pitch and non-loudness 
+                            differences between two sounds.
+                        </label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="timbreLearn" value="1" required>
+                                <label>1</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="timbreLearn" value="2" required>
+                                <label>2</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="timbreLearn" value="3" required>
+                                <label>3</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="timbreLearn" value="4" required>
+                                <label>4</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="timbreLearn" value="5" required>
+                                <label>5</label>
+                            </li>
+                        </ul>
+
+                        <label>
+                            <strong>The process of sound synthesis</strong> - There are many different techniques used to synthesize instruments. Frequency filters 
+                            and amplitude-time envelopes are common effects used in this process to create a desired sound, often applied to a base 
+                            waveform generated by a mathematical function or algorithm. 
+                        </label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="synthLearn" value="1" required>
+                                <label>1</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="synthLearn" value="2" required>
+                                <label>2</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="synthLearn" value="3" required>
+                                <label>3</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="synthLearn" value="4" required>
+                                <label>4</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="synthLearn" value="5" required>
+                                <label>5</label>
+                            </li>
+                        </ul>
+
+                        <label>
+                            <strong>Frequency filters</strong> - Frequency filters modify the amplitude of different frequency components of a single signal. Most 
+                            commonly, they are used to categorically remove either a signal’s high frequency or low-frequency components.       
+                        </label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="filterLearn" value="1" required>
+                                <label>1</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="filterLearn" value="2" required>
+                                <label>2</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="filterLearn" value="3" required>
+                                <label>3</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="filterLearn" value="4" required>
+                                <label>4</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="filterLearn" value="5" required>
+                                <label>5</label>
+                            </li>
+                        </ul>
+
+                        <label>
+                            <strong>The frequency domain / Fourier transforms</strong> - The frequency domain is a way of analyzing signals, where instead of looking 
+                            at how their amplitude changes over time, it looks at what frequency components make up the signal. The Frequency domain is 
+                            typically calculated by applying the Fourier Transform to the signal’s time domain information.     
+                        </label>
+                        <ul class="likert">
+                            <li>
+                                <input type="radio" name="freqLearn" value="1" required>
+                                <label>1</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="freqLearn" value="2" required>
+                                <label>2</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="freqLearn" value="3" required>
+                                <label>3</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="freqLearn" value="4" required>
+                                <label>4</label>
+                            </li>
+                            <li>
+                                <input type="radio" name="freqLearn" value="5" required>
+                                <label>5</label>
+                            </li>
+                        </ul>
+                            
+                    </div>
+
+                    <button type="submit">Submit</button>
+                         
+                    <br><br>
+                    <div class="nextButton">
+                        <a class="nextText" href="/testing3" data-link>Next<a/>
+                    </div>
+
+                </form>
+            </div>
+        `;
+    }
+}
